8e4d193cfffad0b0260861553d844598cc2b7fbc
C.S. Jiang <christine.s.jiang@gmail.com>
2017-05-06 19:13:43 -0400

    index lists and add navigation links to list views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,6 @@
 class ListsController < ApplicationController
   def index
-  	@assignment_one_description = "Complete all missed assignments - Physics"
-	  @assignment_one_pomodoro_estimate = "4 pomodoros"
+  	@lists = List.all
   end
 
   def show
