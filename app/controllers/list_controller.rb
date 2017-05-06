9e0fbdfd584559c82ecdf6a74fc360ceb541d9c1
C.S. Jiang <christine.s.jiang@gmail.com>
2017-05-06 18:30:27 -0400

    add instance variables to List controller and index.html

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,4 +1,9 @@
 class ListController < ApplicationController
   def index
+  	@assignment_one_description = "Complete all missed assignments - Physics"
+	@assignment_one_pomodoro_estimate = "4 pomodoros"
+  end
+
+  def new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'list/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :lists
+  resources :tasks
+
   root 'application#hello'
 end
