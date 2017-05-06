3a88e1a0a3de0547e83a9bf90f0fa26cccefc371
C.S. Jiang <christine.s.jiang@gmail.com>
2017-05-06 19:00:39 -0400

    show and create list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,25 @@
 class ListsController < ApplicationController
   def index
   	@assignment_one_description = "Complete all missed assignments - Physics"
-	@assignment_one_pomodoro_estimate = "4 pomodoros"
+	  @assignment_one_pomodoro_estimate = "4 pomodoros"
+  end
+
+  def show
+    @list = List.find(params[:id])
   end
 
   def new
   end
 
   def create
-  	render plain: params[:list].to_json
+  	@list = List.new(list_params)
+ 
+    @list.save
+    redirect_to @list
   end
+
+  private
+    def list_params
+      params.require(:list).permit(:name, :text)
+    end
 end
