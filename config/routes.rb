3097a3af7746119c3f698c5c698f785a9d5aa276
C.S. Jiang <christine.s.jiang@gmail.com>
2017-05-06 19:13:23 -0400

    replace welcome page and add links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   get 'lists/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :lists
 
-  root 'application#hello'
+  root 'welcome#index'
 end
